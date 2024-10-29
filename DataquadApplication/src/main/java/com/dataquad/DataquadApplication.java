205c3145c7bb0e742b0f149c0f38c25546ba0db3
User <User@ADITLTP026>
2024-10-29 09:42:54 +0530

    Created Project Structure

---
+++ DataquadApplication/src/main/java/com/dataquad/DataquadApplication.java
@@ -0,0 +1,13 @@
+package com.dataquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataquadApplication.class, args);
+	}
+
+}
---
+++ DataquadApplication/src/test/java/com/dataquad/DataquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
